321700d450493a6bfef48b600a4b6872263a6c13
Uriel Torres <uriel09890@gmail.com>
2023-11-19 02:53:28 +0000

    ⭐ Enhance: App Enhancing the app

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/authors', function(_, res) {
+  res.render('authors', { authors: "Jonatan Morales, Uriel Torres" })
+});
+
 module.exports = router;
