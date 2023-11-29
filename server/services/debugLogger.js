f910597dca932725329716db51b105b115ee3db2
Uriel Torres <uriel09890@gmail.com>
2023-11-29 02:34:43 +0000

    ⭐ Enhanced: Modernizando código con ESLINT

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
-import Debug from "debug";
+import Debug from 'debug';
 // Establecemos el proceso a debuggear
-const debug = Debug("innovasync");
+const debug = Debug('innovatesync');
 // exportando el logger de debuggeo
 export default debug;
