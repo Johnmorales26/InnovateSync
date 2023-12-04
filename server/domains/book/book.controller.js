65fa156a7dfd02fa2f57f39c3b655bd3991c1ca6
John Morales <johnta2610@hotmail.com>
2023-12-03 21:14:36 -0600

    ⭐Enhancement: Save new Books

--- server/domains/book/book.controller.js
+++ server/domains/book/book.controller.js
@@ -15,23 +15,44 @@ const addForm = (req, res) => {
 };
 
 // POST "/project/add"
-const addBook = (req, res) => {
-  // Rescatando la info del formulario
+const addBook = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
   // se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
-    res.status(422).json(validationError);
-  } else {
-    // En caso de que pase la validación
-    // Se desestructura la información
-    // de la peticion
-    const { validData: project } = req;
-    // Se contesta la información
-    // del proyecto al cliente
-    res.status(200).json(project);
+    // Se desestructuran los datos de validación
+    // y se renombran de  "value" a "project"
+    const { value: book } = validationError;
+    // Se extraen los campos que fallaron en la validación
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Creando una variable temporal para
+      // evitar el error "no-param-reassing"
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    return res.status(422).render('book/addView', { book, errorModel });
+  }
+  // En caso de que pase la validación
+  // Se desestructura la información
+  // de la peticion
+  const { validData: book } = req;
+  // Creando la instancia de un documento
+  // con los valores de 'project'
+  const bookDocument = new BookModel(book);
+  try {
+    // Se salva el documento en la colección correspondiente
+    const savedBook = await bookDocument.save();
+    // Se contesta la información del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    return res.status(200).json(savedBook);
+  } catch (error) {
+    log.error(
+      'ln 56 project.controller: Error al guardar proyecto en la base de datos',
+    );
+    return res.status(500).json(error);
   }
 };
 
